7d8627ccd2ee23f6d8ae26ff52b02e7738f3f08d
Thilip Rhimo <88575534+thiliprhimo@users.noreply.github.com>
2021-08-08 22:57:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Thanks for visiting Rhimo's application" });
+});
+
+require("./app/routes/garden.routes.js")(app);
+
+app.listen(3000, () => {
+  console.log("Server is running on port : 3000");
+});
